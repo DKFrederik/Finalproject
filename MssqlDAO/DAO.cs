39daec191870b5da83ed8114f7e87d625e066a4d
Nichlas <Nichlas@NICHLAS-PC>
2016-12-05 12:17:15 +0100

    Add database acess methods.

---
+++ MssqlDAO/DAO.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MssqlDAO
+{
+    public class DBAccess
+    {
+        public static string connectionString = @"server=NICHLAS-PC\SQLEXPRESS;" +
+                                      "Trusted_Connection=True;" +
+                                      "MultipleActiveResultSets=True;" +
+                                      "database=Perfion; " +
+                                      "connection timeout=10";
+
+        //public static string connectionString = @"server=FREDERIK-LAPTOP\SQLEXPRESS;" +
+        //                               "Trusted_Connection=True;" +
+        //                               "MultipleActiveResultSets=True;" +
+        //                               "database=Perfion; " +
+        //                               "connection timeout=10";
+
+        //public static string connectionString = @"server=CLAUS-PC\SQLEXPRESS;" +
+        //                               "Trusted_Connection=True;" +
+        //                               "MultipleActiveResultSets=True;" +
+        //                               "database=Perfion; " +
+        //                               "connection timeout=10";
+
+        //public static string connectionString = @"server=ASUS\SQLEXPRESS;" +
+        //                               "Trusted_Connection=True;" +
+        //                               "MultipleActiveResultSets=True;" +
+        //                               "database=Perfion; " +
+        //                               "connection timeout=10";
+
+        private SqlConnection dbconn;
+
+        public DBAccess()
+        {
+            dbconn = new SqlConnection(connectionString);
+        }
+
+        /// <summary>
+        /// Opens a database connection.
+        /// </summary>
+        /// <returns>Returns success if connection was open, else it throws an exception.</returns>
+        public Boolean Open()
+        {
+            Boolean success;
+            try
+            {
+                dbconn.Open();
+                success = true;
+            }
+            catch (Exception ex)
+            {
+                success = false;
+                Console.WriteLine("There was a connection error: " + ex);
+            }
+            return success;
+        }
+
+        /// <summary>
+        /// Closes a database connection.
+        /// </summary>
+        /// <returns>Returns success if connection was closed, else it throws an exception.</returns>
+        public Boolean Close()
+        {
+            Boolean success;
+            try
+            {
+                dbconn.Close();
+                success = true;
+            }
+            catch (Exception ex)
+            {
+                success = false;
+                Console.WriteLine("The connection did not close proberly: " + ex);
+            }
+            return success;
+        }
+
+        /// <summary>
+        /// Creates a new SqlCommand object. If the the connection is not open, the method opens a new connection.
+        /// </summary>
+        /// <param name="sql">A sql string</param>
+        /// <returns>Returns a SqlCommand</returns>
+        public SqlCommand GetDbCommand(string sql)
+        {
+            SqlCommand dbCmd = null;
+            if (dbconn.State.ToString().CompareTo("Open") != 0)
+                Open();
+            if (dbCmd == null)
+            {
+                dbCmd = new SqlCommand(sql, dbconn);
+            }
+            dbCmd.CommandText = sql;
+            return dbCmd;
+        }
+
+        /// <summary>
+        /// Begins a new transaction
+        /// </summary>
+        /// <param name="iso">IsolationLevel</param>
+        /// <returns>Returns a transaction</returns>
+        public SqlTransaction BeginTransaction(IsolationLevel iso)
+        {
+            try
+            {
+                return dbconn.BeginTransaction(iso);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occured: " + ex);
+                return null;
+            }
+        }
+    }
+}
---
+++ TestMssqlDAO/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MssqlDAO;
+
+namespace TestMssqlDAO
+{
+    [TestClass]
+
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ConnectionTest()
+        {
+            DBAccess dba = new DBAccess();
+            Assert.IsTrue(dba.Open());
+            dba.Close();
+        }
+    }
+}
